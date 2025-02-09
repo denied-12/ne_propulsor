94ac811d0d3de3a5188aaf5ad4a58e4569eda668
denied-12 <denied_12@proton.me>
2025-02-09 16:52:19 -0500

    new titlw

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Education Loan System',
+  title: 'Nequ¡-Propulsor',
   description: 'Sistema de educación financiera',
 }
 
